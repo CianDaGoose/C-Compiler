f1d779b513e79b0a526cf9fee9cf0a43c8764c1e
Cian Durnan <cian.durnan2003@gmail.com>
2025-01-13 18:08:50 +0000

    lexer can now take command line input of C files

--- rcc/src/main.rs
+++ rcc/src/main.rs
@@ -1,10 +1,27 @@
 use rcc::Lexer;
 use rcc::Token;
+use std::env;
+use std::fs::File;
+use std::error::Error;
+use std::io::Read;
 
 
-fn main() {
-    let input = "int main() { return 0; }";
-    let mut lexer = Lexer::new(input);
+const SUCCESS: () = ();
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let command_line_arguments: Vec<String> = env::args().collect();
+
+    let target_file = command_line_arguments.get(1);
+
+    let mut file = match target_file {
+        Some(file) => File::open(file)?,
+        None => panic!("No file was provided"),
+    };
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut lexer = Lexer::new(&contents);
 
     while let Some(token) = lexer.next_token() {
         match token {
@@ -12,4 +29,6 @@ fn main() {
             _ => println!("{:?}", token),
         }
     }
+
+    Ok(SUCCESS)
 }
