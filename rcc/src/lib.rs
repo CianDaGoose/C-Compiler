d763498ea4b216a6da62e2a15b77843e111c72cc
Cian Durnan <cian.durnan2003@gmail.com>
2025-01-14 12:52:52 +0000

    lexer has a test suite that can be used by invoking 'cargo test'

--- rcc/src/lib.rs
+++ rcc/src/lib.rs
@@ -121,3 +121,54 @@ fn is_keyword(word: &str) -> bool {
 fn is_symbol(c: char) -> bool {
     matches!(c, '+' | '-' | '*' | '/' | '=' | '<' | '>' | ';' | ',' | '(' | ')' | '{' | '}')
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn is_keyword_valid() {
+        assert_eq!(is_keyword("return"), true, "return should be made a keyword if failed.");
+        assert_eq!(is_keyword("hello"), false, "hello should NOT be a keyword.");
+    }
+
+    #[test]
+    fn is_symbol_valid() {
+        assert_eq!(is_symbol('|'), false, "Pipe is not yet supported as a symbol, should be removed.");
+        assert_eq!(is_symbol(';'), true, "semi-colon should be made a valid symbol.");
+    }
+
+    #[test]
+    fn lexer_basic_operations() -> Result<(), Box<dyn std::error::Error>> {
+        let contents = "int main(void) { return 0; }";
+
+        let mut lexer = Lexer::new(contents);
+        let mut tokens = Vec::new();
+
+        while let Some(token) = lexer.next_token() {
+            match token {
+                Token::Whitespace => continue, // Skip whitespace tokens
+                _ => tokens.push(token),      // Collect other tokens
+            }
+        }
+
+        let expected_tokens = vec![
+            Token::Keyword("int".to_string()),
+            Token::Identifier("main".to_string()),
+            Token::Symbol('('),
+            Token::Keyword("void".to_string()),
+            Token::Symbol(')'),
+            Token::Symbol('{'),
+            Token::Keyword("return".to_string()),
+            Token::Number("0".to_string()),
+            Token::Symbol(';'),
+            Token::Symbol('}'),
+        ];
+
+        assert_eq!(tokens, expected_tokens, "The lexer did not produce the expected tokens.");
+        Ok(())
+    }
+
+}
---
+++ rcc/target/debug/deps/rcc-9f7760176d9881b0.d
@@ -0,0 +1,5 @@
+/home/durnanc2/compiler/C-Compiler/rcc/target/debug/deps/rcc-9f7760176d9881b0: src/lib.rs
+
+/home/durnanc2/compiler/C-Compiler/rcc/target/debug/deps/rcc-9f7760176d9881b0.d: src/lib.rs
+
+src/lib.rs:
